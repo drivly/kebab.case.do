65519a6f9d96f5fa5ee25de294d8c16fbdfa2930
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 14:34:35 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
-import camelcaseKeys from 'camelcase-keys'
+import kebabcaseKeys from 'kebabcase-keys'
 
 export default {
   fetch: async req => {
     const { pathname, search } = new URL(req.url)
     const data = await fetch('https:/' + pathname + search, req).then(res => res.json())
-    return new Response(JSON.stringify(camelcaseKeys(data, { deep: true }), null, 2), { headers: { 'content-type': 'application/json' }})
+    return new Response(JSON.stringify(kebabcaseKeys(data, { deep: true }), null, 2), { headers: { 'content-type': 'application/json' }})
   }
 }
 
